ab7454d6b373e23f1698cb03437cd54d55c11db8
Linx08 <reyeschaveze8@gmail.com>
2025-09-02 17:34:26 -0500

    add port

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ const server = fastify({
   logger: true,
 });
 
+const PORT = process.env.PORT || 3000;
+
 server.get("/", async (request, reply) => {
   return { hello: "world" };
 });
 
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ port: PORT }, (err, address) => {
   if (err) {
     server.log.error(err);
     process.exit(1);
